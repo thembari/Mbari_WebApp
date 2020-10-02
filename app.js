697a9150625aa6af3feffc97cb46631559281919
Damilola Iyiola <dkiyiola@gmail.com>
2020-10-02 11:58:34 +0100

    added fullpage

---
+++ app.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    autoscrolling:true,
+    navigation: true,
+    sectionsColor:['#212360', '#efe2d6', 
+    '#212360','#efe2d6', 
+    '#212360','#efe2d6', 
+    '#212360','#efe2d6','#efe2d6'],
+})
